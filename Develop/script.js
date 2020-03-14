5061d8ded5604fc71c5c5e375768490361d522d7
aphan <phantasticvs@yahoo.com>
2020-03-13 20:29:21 -0600

    As mentioned in the commit for javascript_test, it proved easier to work from a blank slate; therefore, I created the test to get all my code to work and will integrate it with the original homework files.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,35 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+// var generateBtn = document.querySelector("#generate");
+
+// if (confirm("You will be asked a series of questions to determine what kind of password you would like to create, okay?")) {
+
+// }
+
+var lengthSelection = prompt("Your password must be at least 8 characters long and no longer than 128. How many characters would you like your password to have?", "");
+
+console.log(typeof(lengthSelection));
+var lengthSelectionConv = parseInt(lengthSelection);
+console.log(typeof(lengthSelectionConv));
+
+if (lengthSelectionConv < 8 || lengthSelectionConv > 128 || lengthSelectionConv != Number) {
+  // lengthSelection = Number(prompt("Your selection must be a number between 8 and 128. Please re-enter."));
+  // console.log(lengthSelection);
+  alert("shut up");
+} else {
+  alert("bye");
+}
+// while  {
+  
+// }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 <!-- //PURPOSE// -->
   This applet enables a user to randomly generate a secure, unique, and complex password. The interface and experience should be simple, clean, and optimized for multiple viewports.
+
+<!-- //NOTES// -->
+  • During the creation of the listening event for the button element, I wished to trigger a prompt that would allow a selection of one or more options--a check-box scenario--rather than the typical, binary prompt choice. A little research showed that this would be achievable via jQuery; therefore, I decided to abandon this approach in favor of the "series of prompts" mentioned in the assignment instructions.
